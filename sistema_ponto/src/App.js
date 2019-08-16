dd77f0384f68b981955f2c299042aa2c96f7f87e
LuizO <luizdias60@hotmail.com>
2019-08-16 12:56:26 -0300

    Front Home e Cadastro

--- sistema_ponto/src/App.js
+++ sistema_ponto/src/App.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
 import './App.css';
+import './bootstrap.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.showUser = this.showUser.bind(this);
+    
+    this.state = {
+      users: []
+    }
+  }
+
+  async componentDidMount() {
+    let response = await fetch('http://b1a9c387.ngrok.io  /users');
+    let json = await response.json();
+    this.setState({ users: json });
+  }
+
+  showUser(){
+    alert('user');
+  }
+
   render() {
     return (
-      <div className="body"> 
-        <h1>Rodando! </h1>
+      <div className="center_div">
+        <h1>Zuppers</h1>
+        <table className="table table-bordered">
+          <thead>
+            <tr>
+              <th>Nome</th>
+              <th>CPF</th>
+              <th>POD</th>
+              <th>Email</th>
+              <th>Telefone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {        
+              this.state.users.map(function(user){                
+                return (
+                  <tr key={user.cpf}>
+                    <td>{user.nome}</td>
+                    <td>{user.cpf}</td>
+                    <td>{user.pod}</td>
+                    <td>{user.email}</td>
+                    <td>{user.telefone}</td>
+                  </tr>
+                );
+              })
+            }
+          </tbody>
+        </table>
       </div>
     );
   }
--- sistema_ponto/src/Cadastro.js
+++ sistema_ponto/src/Cadastro.js
@@ -1,28 +1,184 @@
 import React, { Component } from 'react';
 import InputMask from 'react-input-mask';
+import './App.css';
+import './bootstrap.css';
+import { Container } from 'react-bootstrap';
 
 class Cadastro extends Component {
-  render() {
-    return (
-      <div className="body"> 
-        <h1>Cadastro </h1>
+    constructor(props){
+        super(props);
+        this.setNome            = this.setNome.bind(this);
+        this.setEmail           = this.setEmail.bind(this);
+        this.setTelefone        = this.setTelefone.bind(this);
+        this.setCPF             = this.setCPF.bind(this);
+        this.setPOD             = this.setPOD.bind(this);
+        this.registerUser       = this.registerUser.bind(this);
+        this.validString        = this.validString.bind(this);
+        this.testaCPF           = this.testaCPF.bind(this);
 
-        <form>
-            <div>
-                <label>Nome: </label> <input type="text"></input>
-            </div>
-            <div>
-                <label>CPF: </label> <InputMask mask="999.999.999-99" maskChar=" " />
-            </div>
-            <div>
-                <label>Email: </label> <input type="email"></input>
-            </div>
-            <div>
-                <label>POD: </label> <input type="text"></input>
-            </div>
-        </form>
-      </div>
-    );
+        this.state = {
+            nome:       '',
+            email:      '',
+            telefone:   '',
+            cpf:        '',
+            pod:        '',
+        }
+    }
+
+    setNome(evento){
+        this.setState({nome: evento.target.value});
+    }
+
+    setEmail(evento){
+        this.setState({email: evento.target.value});
+    }
+    
+    setTelefone(evento){
+        this.setState({telefone: evento.target.value});
+    }
+
+    setCPF(evento){
+        this.setState({cpf: evento.target.value});
+    }
+
+    setPOD(evento){
+        this.setState({pod: evento.target.value});
+    }
+
+    registerUser(event){
+        event.preventDefault();
+        // console.log(this.state.nome);
+        if(
+            !this.testaCPF(this.state.cpf)         ||
+            !this.validString(this.state.nome)     ||
+            !this.validString(this.state.email)    || 
+            !this.validString(this.state.telefone) ||
+            !this.validString(this.state.cpf)      ||
+            !this.validString(this.state.pod)  
+        ){ console.log("alsjdflkjaskdj")
+        return false;}
+        
+        console.log('Cadastro');
+        const requestInfo = {
+            method: 'POST',
+            body: JSON.stringify({
+                nome: this.state.nome, 
+                email: this.state.email, 
+                telefone: this.state.telefone,
+                cpf: this.state.cpf,
+                pod: this.state.pod
+            }),
+            headers: new Headers({
+                'Content-type':'application/json'
+            })
+        };
+    
+        fetch('http://b1a9c387.ngrok.io/users', requestInfo)
+        .then(response => {
+        if(response.ok){
+            this.setState({
+                nome:       '',
+                email:      '',
+                telefone:   '', 
+                cpf:        '', 
+                pod:        ''
+            });          
+            alert('Usuário foi cadastrado com sucesso!');
+            return response.json();
+        } 
+        else {
+            this.setState({
+                nome:       '',
+                email:      '',
+                telefone:   '', 
+                cpf:        '', 
+                pod:        ''
+            });
+            alert('Não foi possível cadastrar o usuário\nErro: "'+response.statusText+'"');
+        }
+        })
+        .then(token => {});
+    }
+    
+    validString(string){
+        if(string === '' || string === null || string === undefined)
+            return false;
+        return true;
+    }
+
+    testaCPF(CPF) {
+        var Soma = 0;
+        var i = 0;
+        var Resto;
+        
+        CPF = CPF.replace(/\D/g, '');
+
+        if (CPF === "00000000000"){ 
+            alert('CPF inválido!');
+            return false;
+        }
+
+        for (i=1; i<=9; i++) 
+            Soma += parseInt(CPF.substring(i-1, i), 10) * (11 - i);
+        Resto = (Soma * 10) % 11;
+       
+        if ((Resto === 10) || (Resto === 11))  
+            Resto = 0;
+
+        if (Resto !== parseInt(CPF.substring(9, 10), 10)){
+            alert('CPF inválido!');
+            return false;
+        }
+
+        Soma = 0;
+
+        for (i = 1; i <= 10; i++) 
+            Soma += parseInt(CPF.substring(i-1, i), 10) * (12 - i);
+        Resto = (Soma * 10) % 11;
+       
+        if ((Resto === 10) || (Resto === 11))  
+            Resto = 0;
+
+        if (Resto !== parseInt(CPF.substring(10, 11), 10)){
+            alert('CPF inválido!');
+            return false;
+        }
+        return true;
+    }
+
+    render() {          
+        return (
+            <Container className="center_div">
+                <div id="container" className="container">
+                    <div className="row text">
+                        <div className="col-sm-6 offset-sm-3 text-center">
+                            <h1>Cadastro </h1>
+                            <div className="info-form">
+                                <form onSubmit={this.registerUser} className="form-inlin justify-content-center">
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" placeholder="Nome" value={this.state.nome} onChange={this.setNome.bind(this)}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" placeholder="Email" value={this.state.email} onChange={this.setEmail.bind(this)}/>
+                                    </div>
+                                    <div className="form-group">
+                                        <InputMask mask="(99)99999-9999" maskChar=" " className="form-control" placeholder="Telefone" value={this.state.telefone} onChange={this.setTelefone.bind(this)}/>                                    
+                                    </div>
+                                    <div className="form-group">
+                                        <InputMask mask="999.999.999-99" maskChar=" " className="form-control" placeholder="CPF" value={this.state.cpf} onChange={this.setCPF.bind(this)}/>                                    
+                                    </div>
+                                    <div className="form-group">
+                                        <input type="text" className="form-control" placeholder="POD" value={this.state.pod} onChange={this.setPOD.bind(this)}/>
+                                    </div>
+                                    <button type="submit" className="btn btn-success ">Cadastrar</button>
+                                </form>
+                            </div>
+                            <br/>
+                        </div>
+                    </div>
+                </div>
+            </Container>
+        );
   }
 }
 
